c00261e3784d08e9f65aab2028b89b0037ab0ba2
taychin <taychin@5lab.co>
2022-11-13 21:57:11 +0700

    re init

---
+++ test/theme/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Test
+ */
+
+get_header();
+?>
+
+<div>
+	tailwind theme is
+	<span class="text-red-300">HERE</span>
+</div>
+<?php
+get_footer();
